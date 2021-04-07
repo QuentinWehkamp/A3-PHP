22273c176a4ef6ca9a651f1f16a6080ab01070a8
QuentinWehkamp <quentinwehkamp@gmail.com>
2021-04-07 11:31:18 +0200

    begin eindopdracht

---
+++ Eindopdrachten/Eindopdracht 1/Huis.class.php
@@ -0,0 +1,13 @@
+<?php
+class Huis {
+    private $Kamers;
+    private $Toiletten;
+    private $Verwarming;
+    private $Verwarming_soort;
+    private $Straatnaam;
+    private $Huisnummer;
+    private $Plaats;
+    private $grond;
+    private $woz;
+}
+?>
